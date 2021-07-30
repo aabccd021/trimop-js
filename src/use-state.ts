04cd62608f7b08248ec79de72763564c191c481b
aabccd021 <aabccd021@gmail.com>
2021-07-30 20:32:08 +0700

    use state

---
+++ src/use-state.ts
@@ -0,0 +1,25 @@
+/* eslint-disable functional/no-return-void */
+/* eslint-disable functional/no-let */
+/* eslint-disable functional/functional-parameters */
+/* eslint-disable functional/immutable-data */
+/* eslint-disable functional/no-expression-statement */
+
+const STATE_RECORD: Record<string, unknown> = {};
+
+let latestStateId = 0;
+
+export type StateController<T> = {
+  readonly get: () => T | undefined;
+  readonly set: (value: T) => void;
+};
+
+export function useState<T>(): StateController<T> {
+  latestStateId += 1;
+  const stateId = latestStateId;
+  return {
+    get: () => STATE_RECORD[stateId] as T,
+    set: (value) => {
+      STATE_RECORD[stateId] = value;
+    },
+  };
+}
