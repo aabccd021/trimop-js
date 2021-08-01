be3dfdfe0af4a6942519d0a98c0f38fa90f1e6ff
aabccd021 <aabccd021@gmail.com>
2021-08-01 14:24:03 +0700

    capital union, always get errorObject

--- src/either.ts
+++ src/either.ts
@@ -4,27 +4,27 @@ import { Failure } from './failure';
  * Value
  */
 export type Value<V> = {
-  readonly _tag: 'value';
+  readonly _tag: 'Value';
   readonly value: V;
 };
 
 export function Value<V>(value: V): Value<V> {
-  return { _tag: 'value', value };
+  return { _tag: 'Value', value };
 }
 
 /**
  * Failed
  */
 export type Failed<F extends Failure> = {
-  readonly _tag: 'failed';
-  readonly errorObject?: Error;
+  readonly _tag: 'Failed';
+  readonly errorObject: Error;
   readonly failure: F;
 };
 
 export function Failed<F extends Failure>(failure: F): Failed<F> {
   return {
-    _tag: 'failed',
-    errorObject: process.env['FP_LITE_TEST'] === 'true' ? undefined : new Error(),
+    _tag: 'Failed',
+    errorObject: new Error(),
     failure,
   };
 }
--- src/fold-value.ts
+++ src/fold-value.ts
@@ -5,5 +5,5 @@ export function foldValue<TResult, F extends Failure = Failure, V = unknown>(
   either: Either<F, V>,
   ifValue: (value: V) => Either<F, TResult>
 ): Either<F, TResult> {
-  return either._tag === 'failed' ? either : ifValue(either.value);
+  return either._tag === 'Failed' ? either : ifValue(either.value);
 }
