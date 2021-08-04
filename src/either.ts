b6337d080ace3e7f72050da9cc9f2e001dbc5b3c
aabccd021 <aabccd021@gmail.com>
2021-08-04 12:23:51 +0700

    create option

--- src/either.ts
+++ src/either.ts
@@ -1,35 +1,120 @@
-import { Failure } from './failure';
-
 /**
- * Value
+ * Right
  */
-export type Value<V> = {
-  readonly _tag: 'Value';
-  readonly value: V;
+export type Right<T> = {
+  readonly _tag: 'Right';
+  readonly right: T;
 };
 
-export function Value<V>(value: V): Value<V> {
-  return { _tag: 'Value', value };
+export function right<T>(right: T): Right<T> {
+  return { _tag: 'Right', right };
 }
 
 /**
- * Failed
+ * Left
  */
-export type Failed<F extends Failure> = {
-  readonly _tag: 'Failed';
+export type Left<E> = {
+  readonly _tag: 'Left';
   readonly errorObject: Error;
-  readonly failure: F;
+  readonly left: E;
 };
 
-export function Failed<F extends Failure>(failure: F): Failed<F> {
+export function left<F>(left: F): Left<F> {
   return {
-    _tag: 'Failed',
+    _tag: 'Left',
     errorObject: new Error(),
-    failure,
+    left,
   };
 }
 
 /**
  * Either
  */
-export type Either<F extends Failure, V> = Failed<F> | Value<V>;
+export type Either<F, V> = Left<F> | Right<V>;
+
+/**
+ *
+ * @param either
+ * @returns
+ */
+export function isRight<T>(either: Either<unknown, T>): either is Right<T> {
+  return either._tag === 'Right';
+}
+
+export function isLeft<E>(either: Either<E, unknown>): either is Left<E> {
+  return either._tag === 'Left';
+}
+
+/**
+ *
+ * @param either
+ * @param mapper
+ * @returns
+ */
+export function eitherMapRight<TResult, F, T>(
+  either: Either<F, T>,
+  mapper: (value: T) => Either<F, TResult>
+): Either<F, TResult> {
+  return either._tag === 'Left' ? either : mapper(either.right);
+}
+
+/**
+ *
+ * @param arr
+ * @param mapper
+ * @param initialValue
+ * @returns
+ */
+export function eitherArrayReduce<TResult, E, T>(
+  arr: readonly Either<E, T>[],
+  initialValue: Either<E, TResult>,
+  mapper: (acc: TResult, value: T, key: string) => Either<E, TResult>
+): Either<E, TResult> {
+  return Object.entries(arr).reduce<Either<E, TResult>>(
+    (acc, [entryKey, entryValue]) =>
+      eitherMapRight(acc, (acc) =>
+        eitherMapRight(entryValue, (entryValue) => mapper(acc, entryValue, entryKey))
+      ),
+    initialValue
+  );
+}
+
+/**
+ *
+ * @param arr
+ * @param mapper
+ * @returns
+ */
+export function eitherArrayFilter<E, T>(
+  arr: readonly Either<E, T>[],
+  mapper: (t: T) => Either<E, boolean>
+): Either<E, readonly T[]> {
+  return arr.reduce<Either<E, readonly T[]>>(
+    (acc, el) =>
+      eitherMapRight(acc, (acc) =>
+        eitherMapRight(el, (el) =>
+          eitherMapRight(mapper(el), (isEqual) => right(isEqual ? [...acc, el] : acc))
+        )
+      ),
+    right([])
+  );
+}
+
+/**
+ *
+ * @param arr
+ * @param mapper
+ * @returns
+ */
+export function eitherArrayMap<TResult, E, T>(
+  arr: readonly Either<E, T>[],
+  mapper: (t: T) => Either<E, TResult>
+): Either<E, readonly TResult[]> {
+  return arr.reduce<Either<E, readonly TResult[]>>(
+    (acc, el) =>
+      eitherMapRight(acc, (acc) =>
+        eitherMapRight(el, (el) => eitherMapRight(mapper(el), (mapped) => right([...acc, mapped])))
+      ),
+    right([])
+  );
+}
